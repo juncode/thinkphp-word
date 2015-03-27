18063f5a80e8102ab45dfaf5312c037d3cd7b2a4
xiaoan <j@wonhsi.com>
2015-03-27 17:26:13 +0800

    完善前端功能集成和flash展示

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,6 +7,9 @@ use Think\Controller;
 class IndexController extends Controller {
 
     public function index() {
+        $model = D('doc');
+        $list = $model->select();
+        $this->assign( 'arrayfile',  $list );
         $this->display( 'index' );
     }
 
@@ -35,18 +38,29 @@ class IndexController extends Controller {
             $this->show('无需要处理的数据' );
             exit(1);
         }
+        echo '总共需要转换' . count( $result ) . '个文件,请稍等。' . '< br />';
         $word_process = new \Think\Word();
         foreach ( $result as $key => $value ) {
+            echo '正在处理第' . ($key + 1) . '个文件：';
             if ( !$word_process->runSwf( $value['uploadfile'] ) ) {
                 $error = $word_process->getError();
                 $updata['err'] = $error['message'];
                 $updata['state'] = $error['code'];
+                echo '转换失败:' . $updata['err'] . '< br />';
                 $word->where( 'id=' . $value['id'] )->save( $updata );
                 continue;
             }
+            echo '转换成功' . '<br />';
             $word->where('id=' . $value['id'] )->save( array ( 'flashname' => $word_process->getSwfName() , 'state' => 1 ) );
         }
-        $this->show('执行完成');
+        echo '执行完成';
+    }
+    
+    public function flash() {
+        $id = I('get.id');
+        $file = ltrim( $id , '.' );
+        $this->assign( 'flashpath', $file );
+        $this->display('flash');
     }
 
 }
