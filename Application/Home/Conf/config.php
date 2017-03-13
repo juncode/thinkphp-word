a7f44125627e2601a6b5a403af553c29db600329
wonhsi <j@wonhsi.com>
2017-03-13 23:32:37 +0800

    Update config.php

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 'DB_HOST' => '192.168.12.210', // 服务器地址
 'DB_NAME' => 'xiaoan', // 数据库名
 'DB_USER' => 'root', // 用户名
-'DB_PWD' => 'antiy?918', // 密码
+'DB_PWD' => '', // 密码
 'DB_PORT' => 3306, // 端口
 'DB_PREFIX' => 'think_', // 数据库表前缀
 'DB_CHARSET'=> 'utf8', // 字符集
